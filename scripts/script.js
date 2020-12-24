a03bd47ddd816c1236f8006a3f6ac767c754a640
Kirill <kirillkalinckin@yandex.ru>
2020-12-24 23:29:05 +0300

    правки после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,25 @@
 let openButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
-
-function togglePopup() {
-popup.classList.toggle('popup_active');
-}
-
-openButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-
-let formElement = popup.querySelector('.popup__container');
-
-function handleFormSubmit (evt) {
-evt.preventDefault();
-
 let nameInput = document.querySelector('[name="name"]');
 let jobInput = document.querySelector('[name="occupation"]');
-
+let formElement = popup.querySelector('.popup__form');
 let name = document.querySelector('.profile__title');
 let job = document.querySelector('.profile__subtitle');
 
-name.textContent = nameInput.value;
-job.textContent = jobInput.value;
-
-togglePopup();
+function togglePopup() {
+  popup.classList.toggle('popup_active');
+  nameInput.value = name.textContent;
+  jobInput.value = job.textContent;
 }
 
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  name.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+  togglePopup();
+}
 
 formElement.addEventListener('submit', handleFormSubmit);
+openButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
