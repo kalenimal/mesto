a1b8cde266b3bb9e67dcf2a386a3a50393d7aaa6
kirill <kirillkalinckin@yandex.ru>
2021-02-03 01:54:37 +0300

    добавить валидацию инпутам

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,63 @@ const cardsList = document.querySelector('.cards__items');
 const imgTitleInput = popupAdd.querySelector('input[name="title"]');
 const imgLinkInput = popupAdd.querySelector('input[name="Link"]');
 const newCard = { name: "", link: "" };
+const configValidation = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}; 
+
+//показывает инпут с ошибкой
+function showInputError (form, input, errorMessage) {
+  const inputError = form.querySelector(`.${input.id}-error`);
+  input.classList.add(configValidation.inputErrorClass);
+  inputError.classList.add(configValidation.errorClass);
+  inputError.textContent = errorMessage;
+}
+
+//показывает  инпут без ошибки
+function hideInputerror (form, input) {
+  const inputError = form.querySelector(`.${input.id}-error`);
+  input.classList.remove(configValidation.inputErrorClass);
+  inputError.classList.remove(configValidation.errorClass);
+  inputError.textContent = "";
+}
+
+//проверяет инпут на валидность 
+function checkValidity (form, input) {
+  if (!input.validity.valid) {
+    showInputError (form, input, input.validationMessage);
+  } else {
+    hideInputerror (form, input);
+  }
+};
+
+
+//навешивает всем инпутам слушатель 
+function setEventListeners (form) {
+  const inputList = Array.from(form.querySelectorAll(configValidation.inputSelector));
+  inputList.forEach((input) => {
+input.addEventListener('input', () => {
+  checkValidity (form, input)
+});
+  });
+};
+
+//вызывают функцию навешивания слушателей инпутам во всех формах
+function enableValidation () {
+  const formList = Array.from(document.querySelectorAll(configValidation.formSelector));
+  formList.forEach((form) => {
+    form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners (form);
+  });
+};
+
+enableValidation ();
 
 //создает карту
 function createCard(el) {
